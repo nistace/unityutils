dacf39ebc749ffcb68c6f1a719cdb93534d0ab15
Nathan <nathan.istace@gmail.com>
2020-08-28 01:06:28 +0200

    Made the fraction a serialized struct

--- Types/Fraction.cs
+++ Types/Fraction.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
+[Serializable]
 public struct Fraction {
-	public int numerator   { get; }
-	public int denominator { get; }
+	[SerializeField] private int _numerator;
+	[SerializeField] private int _denominator;
 
-	public float floatValue => (float) numerator / denominator;
+	public float floatValue => (float) _numerator / _denominator;
 
 	public Fraction(int numerator, int denominator) {
 		var positive = numerator > 0 && denominator > 0 || numerator < 0 && denominator < 0;
@@ -18,24 +20,24 @@ public struct Fraction {
 			}
 		}
 
-		this.numerator = positive ? numeratorAbs : -numeratorAbs;
-		this.denominator = denominatorAbs;
+		_numerator = positive ? numeratorAbs : -numeratorAbs;
+		_denominator = denominatorAbs;
 	}
 
 	public static implicit operator Fraction(int value) => new Fraction(value, 1);
-	public static bool operator ==(Fraction first, Fraction second) => first.numerator == second.numerator && first.denominator == second.denominator;
+	public static bool operator ==(Fraction first, Fraction second) => first._numerator == second._numerator && first._denominator == second._denominator;
 	public static bool operator !=(Fraction first, Fraction second) => !(first == second);
 	public static bool operator <(Fraction first, Fraction second) => first <= second && first != second;
 	public static bool operator >(Fraction first, Fraction second) => first >= second && first != second;
 	public static bool operator <=(Fraction first, Fraction second) => first == second || first.floatValue < second.floatValue;
 	public static bool operator >=(Fraction first, Fraction second) => first == second || first.floatValue > second.floatValue;
-	public static Fraction operator +(Fraction f, Fraction s) => new Fraction(f.numerator * s.denominator + s.numerator * f.denominator, f.denominator * s.denominator);
-	public static Fraction operator -(Fraction fraction) => new Fraction(-fraction.numerator, fraction.denominator);
+	public static Fraction operator +(Fraction f, Fraction s) => new Fraction(f._numerator * s._denominator + s._numerator * f._denominator, f._denominator * s._denominator);
+	public static Fraction operator -(Fraction fraction) => new Fraction(-fraction._numerator, fraction._denominator);
 	public static Fraction operator -(Fraction f, Fraction s) => f + -s;
-	public static Fraction operator *(Fraction f, Fraction s) => new Fraction(f.numerator * s.numerator, f.denominator * s.denominator);
-	public static Fraction operator /(Fraction f, Fraction s) => new Fraction(f.numerator * s.denominator, f.denominator * s.numerator);
+	public static Fraction operator *(Fraction f, Fraction s) => new Fraction(f._numerator * s._numerator, f._denominator * s._denominator);
+	public static Fraction operator /(Fraction f, Fraction s) => new Fraction(f._numerator * s._denominator, f._denominator * s._numerator);
 
-	private bool Equals(Fraction other) => numerator == other.numerator && denominator == other.denominator;
+	private bool Equals(Fraction other) => _numerator == other._numerator && _denominator == other._denominator;
 
 	public override bool Equals(object obj) {
 		switch (obj) {
@@ -47,5 +49,5 @@ public struct Fraction {
 
 	public override int GetHashCode() => floatValue.GetHashCode();
 
-	public string ToString(string format = "n/d") => format.Replace("n", $"{numerator}").Replace("d", $"{denominator}");
+	public string ToString(string format = "n/d") => format.Replace("n", $"{_numerator}").Replace("d", $"{_denominator}");
 }
