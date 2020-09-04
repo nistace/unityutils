fff88251343e95c2c95cc597f05352333de0cac1
Nathan <nathan.istace@gmail.com>
2020-09-05 01:42:24 +0200

    Fixed formatting

--- Types/IntRange.cs
+++ Types/IntRange.cs
@@ -3,14 +3,6 @@ using UnityEngine;
 
 [Serializable]
 public struct IntRange {
-	public override bool Equals(object obj) {
-		return obj is IntRange other && Equals(other);
-	}
-
-	public override int GetHashCode() {
-		return (min * 397) ^ max;
-	}
-
 	public static readonly IntRange zero = new IntRange {_min = 0, _max = 0};
 	public static readonly IntRange one  = new IntRange {_min = 1, _max = 1};
 	public static readonly IntRange unit = new IntRange {_min = 0, _max = 1};
@@ -27,39 +19,22 @@ public struct IntRange {
 		_max = max;
 	}
 
-	public int Random() {
-		return UnityEngine.Random.Range(_min, _max + 1);
-	}
-
-	public float FloatRandom() {
-		return UnityEngine.Random.Range((float) _min, _max);
-	}
+	public int Random() => UnityEngine.Random.Range(_min, _max + 1);
+	public float FloatRandom() => UnityEngine.Random.Range((float) _min, _max);
 
 	public static IntRange operator *(IntRange orig, int amount) => new IntRange(orig._min * amount, orig._max * amount);
 
-	public static FloatRange operator *(IntRange orig, float amount) {
-		return new FloatRange(orig._min * amount, orig._max * amount);
-	}
+	public static FloatRange operator *(IntRange orig, float amount) => new FloatRange(orig._min * amount, orig._max * amount);
 
-	public static IntRange operator +(IntRange ir1, IntRange ir2) {
-		return new IntRange(ir1._min + ir2._min, ir1._max + ir2._max);
-	}
+	public static IntRange operator +(IntRange ir1, IntRange ir2) => new IntRange(ir1._min + ir2._min, ir1._max + ir2._max);
 
-	public static IntRange operator -(IntRange ir1, IntRange ir2) {
-		return new IntRange(ir1._min - ir2._min, ir1._max - ir2._max);
-	}
+	public static IntRange operator -(IntRange ir1, IntRange ir2) => new IntRange(ir1._min - ir2._min, ir1._max - ir2._max);
 
-	public static bool operator ==(IntRange ir1, IntRange ir2) {
-		return ir1.Equals(ir2);
-	}
+	public static bool operator ==(IntRange ir1, IntRange ir2) => ir1.Equals(ir2);
 
-	public static bool operator !=(IntRange ir1, IntRange ir2) {
-		return !(ir1 == ir2);
-	}
+	public static bool operator !=(IntRange ir1, IntRange ir2) => !(ir1 == ir2);
 
-	private bool Equals(IntRange other) {
-		return min == other.min && max == other.max;
-	}
+	private bool Equals(IntRange other) => min == other.min && max == other.max;
 
 	/// <summary>if both values are equal, returns the value, otherwise returns a "m - M" formatted string</summary>
 	public string ToRangeString() {
@@ -67,11 +42,11 @@ public struct IntRange {
 		return $"{min} - {max}";
 	}
 
-	public int Clamped(int value) {
-		return value.Clamp(min, max);
-	}
+	public int Clamped(int value) => value.Clamp(min, max);
 
-	public bool Contains(int value) {
-		return value <= max && value >= min;
-	}
+	public bool Contains(int value) => value <= max && value >= min;
+
+	public override bool Equals(object obj) => obj is IntRange other && Equals(other);
+
+	public override int GetHashCode() => (min * 397) ^ max;
 }
