5309d77d5a62799115a330b3a934f94b7c2cf8e8
Nathan <nathan.istace@gmail.com>
2020-11-03 09:16:54 +0100

    Debug utils

---
+++ Debugging/Debug.cs
@@ -0,0 +1,8 @@
+﻿namespace Utils.Debugging {
+	public static class Debug {
+		public static void Log(string info) {
+			UnityEngine.Debug.Log(info);
+			DebugUi.Print(info);
+		}
+	}
+}
---
+++ Debugging/DebugUi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class DebugUi : MonoBehaviour {
+	private static DebugUi instance { get; set; }
+
+	[SerializeField] protected TMP_Text  _linePrefab;
+	[SerializeField] protected Transform _linesContainer;
+
+	private void Awake() {
+		if (instance) Destroy(gameObject);
+		else instance = this;
+		if (transform != transform.root) transform.SetParent(null);
+		DontDestroyOnLoad(gameObject);
+	}
+
+	private void Start() {
+		gameObject.SetActive(false);
+	}
+
+	public static void Print(string info) {
+		var line = Instantiate(instance._linePrefab, instance._linesContainer);
+		line.SetText($"[{DateTime.Now:h:mm:ss tt}]{info}");
+	}
+}
