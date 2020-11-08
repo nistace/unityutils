31d954e888feed00632d03b39963ce18c94e85d5
Nathan <nathan.istace@gmail.com>
2020-11-08 17:43:42 +0100

    Added the RoomExtension to define Custom properties ergo the player extension ; Refactored and added possibility to save and load a hashtable

--- Extensions/PunHashtableExtension.cs
+++ Extensions/PunHashtableExtension.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using ExitGames.Client.Photon;
 using UnityEngine;
 
 namespace Utils.Extensions {
 	public static class PunHashtableExtension {
-		public static int Int(this Hashtable table, string name, int defaultValue = default) => table.Get(name, defaultValue);
-		public static byte Byte(this Hashtable table, string name, byte defaultValue = default) => table.Get(name, defaultValue);
-		public static string String(this Hashtable table, string name, string defaultValue = default) => table.Get(name, defaultValue);
-		public static float Float(this Hashtable table, string name, float defaultValue = default) => table.Get(name, defaultValue);
-		public static bool Bool(this Hashtable table, string name, bool defaultValue = default) => table.Get(name, defaultValue);
+		public static int Int(this Hashtable table, string name, int defaultValue = default) => table.TryInt(name, out var value) ? value : defaultValue;
+		public static bool TryInt(this Hashtable table, string name, out int value) => table.TryGet(name, out value, int.Parse);
+		public static byte Byte(this Hashtable table, string name, byte defaultValue = default) => table.TryByte(name, out var value) ? value : defaultValue;
+		public static bool TryByte(this Hashtable table, string name, out byte value) => table.TryGet(name, out value, byte.Parse);
+		public static string String(this Hashtable table, string name, string defaultValue = default) => table.TryString(name, out var value) ? value : defaultValue;
+		public static bool TryString(this Hashtable table, string name, out string value) => table.TryGet(name, out value, t => t);
+		public static float Float(this Hashtable table, string name, float defaultValue = default) => table.TryFloat(name, out var value) ? value : defaultValue;
+		public static bool TryFloat(this Hashtable table, string name, out float value) => table.TryGet(name, out value, float.Parse);
+		public static bool Bool(this Hashtable table, string name, bool defaultValue = default) => table.TryBool(name, out var value) ? value : defaultValue;
+		public static bool TryBool(this Hashtable table, string name, out bool value) => table.TryGet(name, out value, bool.Parse);
 		public static string[] StringArray(this Hashtable table, string key) => table.Array(key, t => t);
 		public static int[] IntArray(this Hashtable table, string key) => table.Array(key, int.Parse);
 		public static float[] FloatArray(this Hashtable table, string key) => table.Array(key, float.Parse);
@@ -36,16 +42,23 @@ namespace Utils.Extensions {
 			return new Vector2(asArray[0], asArray[1]);
 		}
 
-		public static bool TryGet<E>(this Hashtable table, string name, out E value) {
-			if (table.ContainsKey(name) && table[name] is E castValue) {
+		private static bool TryGet<E>(this Hashtable table, string name, out E value, Func<string, E> parseFunc) {
+			value = default;
+			if (!table.ContainsKey(name)) return false;
+			if (table[name] is E castValue) {
 				value = castValue;
 				return true;
 			}
-			value = default;
+			if (parseFunc == null) return false;
+			if (!(table[name] is string strValue)) return false;
+			try {
+				value = parseFunc.Invoke(strValue);
+				table[name] = value;
+				return true;
+			}
+			catch (FormatException) { }
 			return false;
 		}
-
-		private static E Get<E>(this Hashtable table, string name, E defaultValue = default) => table.TryGet<E>(name, out var castValue) ? castValue : defaultValue;
 
 		public static void Define(this Hashtable table, string key, int i) => table[key] = i;
 		public static void Define(this Hashtable table, string key, float f) => table[key] = f;
@@ -58,5 +71,38 @@ namespace Utils.Extensions {
 		public static void Define(this Hashtable table, string key, IEnumerable<string> array) => table.Define(key, array.Join("|"));
 		public static void Define(this Hashtable table, string key, IEnumerable<int> array) => table.Define(key, array.Join("|"));
 		public static void Define(this Hashtable table, string key, IEnumerable<float> array) => table.Define(key, array.Join("|"));
+
+		private const int  fileCharOffset = 32;
+		private const char fileSeparator  = (char) 0;
+
+		public static void Save(this Hashtable table, string path) {
+			using (var o = File.CreateText(path)) {
+				var previousKey = string.Empty;
+				foreach (var key in table.Keys.Select(t => t.ToString()).OrderBy(t => t)) {
+					var copyPrevious = 0;
+					while (copyPrevious < previousKey.Length && copyPrevious < key.Length && previousKey[copyPrevious] == key[copyPrevious]) copyPrevious++;
+					o.Write($"{(char) (copyPrevious + fileCharOffset)}{key.Substring(copyPrevious)}={table[key]}{fileSeparator}");
+					previousKey = key;
+				}
+			}
+		}
+
+		public static Hashtable Load(this Hashtable table, string path) {
+			using (var o = File.OpenText(path)) {
+				var text = o.ReadToEnd();
+				var lines = text.Split(fileSeparator);
+				var previousKey = string.Empty;
+				foreach (var line in lines) {
+					if (string.IsNullOrEmpty(line)) continue;
+					var keyPart = line.Substring(0, line.IndexOf('='));
+					var copyPrevious = keyPart[0] - fileCharOffset;
+					var key = previousKey.Substring(0, copyPrevious) + keyPart.Substring(1);
+					var value = line.Substring(line.IndexOf('=') + 1);
+					table[key] = value;
+					previousKey = key;
+				}
+			}
+			return table;
+		}
 	}
 }
---
+++ Extensions/PunRoomExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Utils.Extensions {
+	public static class PunRoomExtension {
+		public static void SetCustomProperty(this Room room, string key, int value) => room.SetCustomProperty(t => t.Define(key, value));
+		public static void SetCustomProperty(this Room room, string key, string value) => room.SetCustomProperty(t => t.Define(key, value));
+		public static void SetCustomProperty(this Room room, string key, float value) => room.SetCustomProperty(t => t.Define(key, value));
+		public static void SetCustomProperty(this Room room, string key, bool value) => room.SetCustomProperty(t => t.Define(key, value));
+		public static void SetCustomProperty(this Room room, string key, IEnumerable<int> value) => room.SetCustomProperty(t => t.Define(key, value));
+		public static void SetCustomProperty(this Room room, string key, IEnumerable<float> value) => room.SetCustomProperty(t => t.Define(key, value));
+		public static void SetCustomProperty(this Room room, string key, Vector2 value) => room.SetCustomProperty(t => t.Define(key, value));
+		public static void SetCustomProperty(this Room room, string key, Vector3 value) => room.SetCustomProperty(t => t.Define(key, value));
+		public static void SetCustomProperty(this Room room, string key, Color value) => room.SetCustomProperty(t => t.Define(key, value));
+
+		private static void SetCustomProperty(this Room room, Action<Hashtable> defineFunc) {
+			var data = new Hashtable();
+			defineFunc(data);
+			room.SetCustomProperties(data);
+		}
+	}
+}
