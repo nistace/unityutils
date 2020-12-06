71bb655960afe2bb92b11b129fe80b3cac279811
Nathan <nathan.istace@gmail.com>
2020-12-06 21:03:49 +0100

    Parsed values are saved to avoid parsing multiple times

--- Libraries/Constants/Constants.cs
+++ Libraries/Constants/Constants.cs
@@ -1,15 +1,25 @@
-﻿public static class Constants {
-	private static ConstantLibrary library { get; set; }
-	public static  bool            loaded  => library != null;
+﻿using System;
+using System.Collections.Generic;
+
+public static class Constants {
+	private static ConstantLibrary           library          { get; set; }
+	public static  bool                      loaded           => library != null;
+	private static Dictionary<string, int>   parsedToInts     { get; } = new Dictionary<string, int>();
+	private static Dictionary<string, float> parsedToFloats   { get; } = new Dictionary<string, float>();
+	private static Dictionary<string, bool>  parsedToBooleans { get; } = new Dictionary<string, bool>();
 
 	public static void LoadLibrary(ConstantLibrary libraryToLoad) {
 		library = libraryToLoad;
 		if (library) library.Load();
 	}
 
-	public static int Int(string name, int defaultValue = 0) => int.TryParse(String(name), out var value) ? value : defaultValue;
-	public static float Float(string name, float defaultValue = 0) => float.TryParse(String(name).Replace('.', ','), out var value) ? value : defaultValue;
-	public static bool Bool(string name, bool defaultValue = false) => bool.TryParse(String(name), out var value) ? value : defaultValue;
-
+	public static int Int(string name, int defaultValue = 0) => Get(name, parsedToInts, t => int.TryParse(t, out var value) ? value : defaultValue);
+	public static float Float(string name, float defaultValue = 0) => Get(name, parsedToFloats, t => float.TryParse(t.Replace('.', ','), out var value) ? value : defaultValue);
+	public static bool Bool(string name, bool defaultValue = false) => Get(name, parsedToBooleans, t => bool.TryParse(t, out var value) ? value : defaultValue);
 	public static string String(string name) => library?[name] ?? string.Empty;
+
+	private static E Get<E>(string key, IDictionary<string, E> parsedValues, Func<string, E> parseFunc) {
+		if (!parsedValues.ContainsKey(key)) parsedValues.Add(key, parseFunc(String(key)));
+		return parsedValues[key];
+	}
 }
