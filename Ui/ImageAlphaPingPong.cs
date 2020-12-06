0d2cc6509e3cf09f6020fe7d1008f0295a5a5612
Nathan <nathan.istace@gmail.com>
2020-12-06 21:05:20 +0100

    Created a class to blink an image color alpha

---
+++ Ui/ImageAlphaPingPong.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utils.Ui {
+	[RequireComponent(typeof(Image))]
+	public class ImageAlphaPingPong : MonoBehaviour {
+		[SerializeField] protected Image _image;
+		[SerializeField] protected float _minAlpha;
+		[SerializeField] protected float _maxAlpha = 1;
+		[SerializeField] protected float _speed    = 1;
+
+		private float lerp          { get; set; }
+		private float lerpDirection { get; set; } = 1;
+
+		private void Reset() {
+			_image = GetComponent<Image>();
+		}
+
+		private void Update() {
+			lerp += lerpDirection * Time.deltaTime * _speed;
+			if (lerp <= 0) lerpDirection = 1;
+			else if (lerp >= 1) lerpDirection = -1;
+			_image.color = _image.color.With(a: Mathf.Lerp(_minAlpha, _maxAlpha, lerp));
+		}
+	}
+}
