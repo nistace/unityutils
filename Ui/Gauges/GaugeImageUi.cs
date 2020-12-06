2677c3c34099474f9336d1f1faf9a08e5fdd0193
Nathan <nathan.istace@gmail.com>
2020-12-06 21:04:29 +0100

    Gauge utils

---
+++ Ui/Gauges/GaugeImageUi.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utils.Ui.Gauges {
+	[RequireComponent(typeof(Image))]
+	public class GaugeImageUi : MonoBehaviourUi {
+		[SerializeField] protected Image _image;
+
+		public float fillAmount {
+			get => _image.fillAmount;
+			set => _image.fillAmount = value;
+		}
+
+		public Color color {
+			get => _image.color;
+			set => _image.color = value;
+		}
+
+		private void Reset() {
+			_image = GetComponent<Image>();
+		}
+
+		public void SlowlyChangeFill(float targetFill, float delayBeforeStart, float time) {
+			StartSingleCoroutine(DoSlowlyChangeFill(targetFill, delayBeforeStart, time));
+		}
+
+		private IEnumerator DoSlowlyChangeFill(float targetFill, float delayBeforeStart, float time) {
+			for (var delayProgress = 0f; delayProgress < delayBeforeStart; delayProgress += Time.deltaTime) yield return null;
+			if (time > 0) {
+				var stepToTarget = Mathf.Abs(targetFill - fillAmount) / time;
+				while (fillAmount != targetFill) {
+					fillAmount = Mathf.MoveTowards(fillAmount, targetFill, stepToTarget * Time.deltaTime);
+					yield return null;
+				}
+			}
+			fillAmount = targetFill;
+		}
+	}
+}
---
+++ Ui/Gauges/TwoLayerGaugeUi.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Utils.Ui.Gauges {
+	public class TwoLayerGaugeUi : MonoBehaviourUi {
+		[SerializeField] protected GaugeImageUi _frontGauge;
+		[SerializeField] protected GaugeImageUi _backGauge;
+
+		public GaugeImageUi frontGauge => _frontGauge;
+		public GaugeImageUi backGauge  => _backGauge;
+
+		public float fillAmount {
+			get => _frontGauge.fillAmount;
+			set {
+				_backGauge.fillAmount = value;
+				_frontGauge.fillAmount = value;
+			}
+		}
+
+		public void SlowlyChangeFill(float targetFill, float delayBeforeStart, float time) {
+			if (_frontGauge.fillAmount == targetFill) return;
+			if (_frontGauge.fillAmount > targetFill) {
+				_backGauge.fillAmount = _frontGauge.fillAmount;
+				_frontGauge.fillAmount = targetFill;
+				_backGauge.SlowlyChangeFill(targetFill, delayBeforeStart, time);
+			}
+			else {
+				_backGauge.fillAmount = targetFill;
+				_frontGauge.SlowlyChangeFill(targetFill, delayBeforeStart, time);
+			}
+		}
+	}
+}
