a21fd22d2a20a121d4648b2b11f046291860b361
Nathan <nathan.istace@gmail.com>
2020-12-06 21:02:34 +0100

    Reviewed UI components to work with coroutines instead of update function

--- Ui/ExpandableUi.cs
+++ Ui/ExpandableUi.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 using Utils.Types.Ui;
+using Utils.Ui;
 
-public class ExpandableUi : MonoBehaviour {
+public class ExpandableUi : MonoBehaviourUi {
 	protected enum ExpandableDirection {
 		All        = 0,
 		Vertical   = 1,
@@ -14,78 +17,79 @@ public class ExpandableUi : MonoBehaviour {
 	[SerializeField] protected RectTransformPosition _expandedPosition   = new RectTransformPosition();
 	[SerializeField] protected Button                _expandButton;
 	[SerializeField] protected Button                _contractButton;
-	[SerializeField] protected float                 _smoothness = .5f;
+	[SerializeField] protected float                 _defaultSmoothness = .5f;
 	[SerializeField] protected bool                  _initiallyExpanded;
 
-	private float   resizeLerp           { get; set; }
-	private Vector2 destinationOffsetMin { get; set; }
-	private Vector2 destinationOffsetMax { get; set; }
-
-	private new RectTransform transform { get; set; }
-
 	private void Awake() {
-		transform = GetComponent<RectTransform>();
 		_expandButton.onClick.AddListenerOnce(Expand);
 		_contractButton.onClick.AddListenerOnce(Contract);
-		if (_initiallyExpanded) Expand();
-		else Contract();
+		if (_initiallyExpanded) Expand(0);
+		else Contract(0);
 	}
 
-	private void Contract() {
-		SetDestination(_contractedPosition);
+	private void Contract() => Contract(null);
+
+	private void Contract(float? smoothness) {
+		MoveTo(_contractedPosition, smoothness ?? _defaultSmoothness);
 		_contractButton.gameObject.SetActive(false);
 		_expandButton.gameObject.SetActive(true);
 	}
 
-	private void Expand() {
-		SetDestination(_expandedPosition);
+	private void Expand() => Expand(null);
+
+	private void Expand(float? smoothness) {
+		MoveTo(_expandedPosition, smoothness ?? _defaultSmoothness);
 		_contractButton.gameObject.SetActive(true);
 		_expandButton.gameObject.SetActive(false);
 	}
 
-	private void SetDestination(RectTransformPosition destination) {
-		var destinationAnchorMin = new Vector2(_direction.In(ExpandableDirection.All, ExpandableDirection.Horizontal) ? destination.anchorMin.x : transform.anchorMin.x,
-			_direction.In(ExpandableDirection.All, ExpandableDirection.Vertical) ? destination.anchorMin.y : transform.anchorMin.y);
-		var destinationAnchorMax = new Vector2(_direction.In(ExpandableDirection.All, ExpandableDirection.Horizontal) ? destination.anchorMax.x : transform.anchorMax.x,
-			_direction.In(ExpandableDirection.All, ExpandableDirection.Vertical) ? destination.anchorMax.y : transform.anchorMax.y);
-		transform.MoveAnchorsKeepPosition(destinationAnchorMin, destinationAnchorMax);
-		destinationOffsetMin = new Vector2(_direction.In(ExpandableDirection.All, ExpandableDirection.Horizontal) ? destination.offset.left : transform.offsetMin.x,
-			_direction.In(ExpandableDirection.All, ExpandableDirection.Vertical) ? destination.offset.bottom : transform.offsetMin.y);
-		destinationOffsetMax = new Vector2(_direction.In(ExpandableDirection.All, ExpandableDirection.Horizontal) ? destination.offset.right : transform.offsetMax.x,
-			_direction.In(ExpandableDirection.All, ExpandableDirection.Vertical) ? destination.offset.top : transform.offsetMax.y);
-		if (_smoothness == 0) SetLerp(1);
-		else resizeLerp = 0;
+	private void MoveTo(RectTransformPosition destination, float smoothness) {
+		if (smoothness <= 0) JumpTo(destination);
+		else StartSingleCoroutine(DoMovement(destination, smoothness));
 	}
 
-	private void Update() {
-		if (resizeLerp >= 1) return;
-		if (_smoothness == 0) SetLerp(1);
-		else SetLerp(resizeLerp + Time.deltaTime / _smoothness);
+	private void JumpTo(RectTransformPosition destination) {
+		transform.MoveAnchorsKeepPosition(TransformWithDirection(destination.anchorMin, transform.anchorMin), TransformWithDirection(destination.anchorMax, transform.anchorMax));
+		transform.offsetMin = TransformWithDirection(destination.offsetMin, transform.offsetMin);
+		transform.offsetMax = TransformWithDirection(destination.offsetMax, transform.offsetMax);
 	}
 
-	private void SetLerp(float lerp) {
-		resizeLerp = lerp.Clamp(0, 1);
-		transform.offsetMin = Vector2.Lerp(transform.offsetMin, destinationOffsetMin, resizeLerp);
-		transform.offsetMax = Vector2.Lerp(transform.offsetMax, destinationOffsetMax, resizeLerp);
+	private IEnumerator DoMovement(RectTransformPosition destination, float smoothness) {
+		var destinationOffsetMin = TransformWithDirection(destination.offsetMin, transform.offsetMin);
+		var destinationOffsetMax = TransformWithDirection(destination.offsetMax, transform.offsetMax);
+		transform.MoveAnchorsKeepPosition(TransformWithDirection(destination.anchorMin, transform.anchorMin), TransformWithDirection(destination.anchorMax, transform.anchorMax));
+
+		if (smoothness > 0) {
+			var timeCoefficient = 1 / smoothness;
+			for (var lerp = 0f; lerp <= 1; lerp += Time.deltaTime * timeCoefficient) {
+				transform.offsetMin = Vector2.Lerp(transform.offsetMin, destinationOffsetMin, lerp);
+				transform.offsetMax = Vector2.Lerp(transform.offsetMax, destinationOffsetMax, lerp);
+				yield return null;
+			}
+		}
+		transform.offsetMin = destinationOffsetMin;
+		transform.offsetMax = destinationOffsetMax;
 	}
 
-	[ContextMenu("Save expanded values")]
-	private void SaveExpandedValues() {
-		if (!transform) transform = GetComponent<RectTransform>();
-		_expandedPosition.anchorMin = transform.anchorMin;
-		_expandedPosition.anchorMax = transform.anchorMax;
-		var offsetMax = transform.offsetMax;
-		var offsetMin = transform.offsetMin;
-		_expandedPosition.offset = new RectOffset((int) offsetMin.x, (int) offsetMax.x, (int) offsetMax.y, (int) offsetMin.y);
+	private Vector2 TransformWithDirection(Vector2 expected, Vector2 current) {
+		switch (_direction) {
+			case ExpandableDirection.All: return expected;
+			case ExpandableDirection.Horizontal: return expected.With(y: current.y);
+			case ExpandableDirection.Vertical: return expected.With(x: current.x);
+			default: throw new ArgumentOutOfRangeException();
+		}
 	}
 
-	[ContextMenu("Save contracted values")]
-	private void SaveContractedValues() {
-		if (!transform) transform = GetComponent<RectTransform>();
-		_contractedPosition.anchorMin = transform.anchorMin;
-		_contractedPosition.anchorMax = transform.anchorMax;
-		var offsetMax = transform.offsetMax;
-		var offsetMin = transform.offsetMin;
-		_contractedPosition.offset = new RectOffset((int) offsetMin.x, (int) offsetMax.x, (int) offsetMax.y, (int) offsetMin.y);
+#if UNITY_EDITOR
+
+	private void SaveCurrent(RectTransformPosition inPosition) {
+		inPosition.anchorMin = transform.anchorMin;
+		inPosition.anchorMax = transform.anchorMax;
+		inPosition.offsetMax = transform.offsetMax;
+		inPosition.offsetMin = transform.offsetMin;
 	}
+
+	[ContextMenu("Save expanded values")] private void SaveExpandedValues() => SaveCurrent(_expandedPosition);
+	[ContextMenu("Save contracted values")] private void SaveContractedValues() => SaveCurrent(_contractedPosition);
+#endif
 }
--- Ui/ScrollRectAutoScroller.cs
+++ Ui/ScrollRectAutoScroller.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Utils.Types.Ui {
@@ -7,24 +8,20 @@ namespace Utils.Types.Ui {
 		[SerializeField] protected Scrollbar  _scrollbar;
 		[SerializeField] protected int        _duringFrames = 2;
 
-		public  bool  atBottom        => _scrollbar.value < .001f;
-		public  bool  atTop           => _scrollbar.value > .999f;
-		private int   remainingFrames { get; set; }
-		private float target          { get; set; }
+		public bool atBottom => _scrollbar.value < .001f;
+		public bool atTop    => _scrollbar.value > .999f;
 
 		public void ScrollToBottom() => ScrollTo(0, _duringFrames);
 
 		public void ScrollToTop() => ScrollTo(1, _duringFrames);
 
-		private void ScrollTo(float value, int duringFrames) {
-			target = value;
-			remainingFrames = duringFrames;
-		}
+		private void ScrollTo(float value, int duringFrames) => StartCoroutine(DoScrollTo(value, duringFrames));
 
-		private void LateUpdate() {
-			if (remainingFrames <= 0) return;
-			_scrollRect.verticalNormalizedPosition = target;
-			remainingFrames--;
+		private IEnumerator DoScrollTo(float value, int duringFrames) {
+			for (var i = 0; i < duringFrames; ++i) {
+				yield return null;
+				_scrollRect.verticalNormalizedPosition = value;
+			}
 		}
 	}
 }
