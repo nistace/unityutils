7fe154ee0b3fda8cbf35e2186f87861e20ee34e7
Nathan <nathan.istace@gmail.com>
2020-12-06 21:04:58 +0100

    Utils class to build bars with separators

---
+++ Ui/HorizontalSeparatedBarUi.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utils.Ui {
+	public class HorizontalSeparatedBarUi : MonoBehaviourUi {
+		[SerializeField] protected int         _parts;
+		[SerializeField] protected Color       _separatorColor;
+		[SerializeField] protected float       _separatorWidth;
+		[SerializeField] protected List<Image> _separators = new List<Image>();
+
+		public void Build(int parts) {
+			_parts = parts;
+			Build();
+		}
+
+		[ContextMenu("Build")]
+		public void Build() {
+			while (_separators.Count < _parts - 1) {
+				var newSeparator = new GameObject("Separator (Auto generated)").ParentedTo(transform).AddComponent<Image>();
+				newSeparator.color = _separatorColor;
+				_separators.Add(newSeparator);
+			}
+
+			for (var i = 0; i < _parts - 1; ++i) {
+				_separators[i].gameObject.SetActive(true);
+				_separators[i].rectTransform.anchorMin = new Vector2((float) (i + 1) / _parts, 0);
+				_separators[i].rectTransform.anchorMax = new Vector2((float) (i + 1) / _parts, 1);
+				_separators[i].rectTransform.offsetMin = new Vector2(-_separatorWidth / 2f, 0);
+				_separators[i].rectTransform.offsetMax = new Vector2(_separatorWidth / 2f, 0);
+			}
+
+			for (var i = _parts - 1; i < _separators.Count; ++i) _separators[i].gameObject.SetActive(false);
+		}
+	}
+}
