5bb24e17382016ebacbff4c278d0ebede5251fda
Nathan <nathan.istace@gmail.com>
2020-12-14 16:53:36 +0100

    PanelUi

---
+++ Ui/PanelUi.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Utils.Ui {
+	[RequireComponent(typeof(PanelTransformUi))]
+	public abstract class PanelUi : MonoBehaviourUi {
+		private PanelTransformUi pt             { get; set; }
+		public  PanelTransformUi panelTransform => pt ? pt : pt = GetComponent<PanelTransformUi>();
+	}
+}
