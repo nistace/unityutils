82e256cb63a2bc1ba5bd963fcde3cfe8ac0a2e8b
Nathan <nathan.istace@gmail.com>
2020-12-20 17:06:44 +0100

    Added a requirement over the MonoBehaviourMyPun for a PhotonView

--- Behaviours/MonoBehaviourMyPun.cs
+++ Behaviours/MonoBehaviourMyPun.cs
@@ -1,6 +1,8 @@
 ﻿using Photon.Pun;
+using UnityEngine;
 
 namespace Utils.Behaviours {
+	[RequireComponent(typeof(PhotonView))]
 	public class MonoBehaviourMyPun : MonoBehaviourPun {
 		public bool isMine => photonView.IsMine || Equals(photonView.Controller, PhotonNetwork.LocalPlayer);
 	}
