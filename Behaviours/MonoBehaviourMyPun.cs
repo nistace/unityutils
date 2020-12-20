405e10856d356f606d756acc591c14e61ee21ead
Nathan <nathan.istace@gmail.com>
2020-12-20 13:10:47 +0100

    Create an additional layer to MonoBehaviourPun to fix the IsMine value

---
+++ Behaviours/MonoBehaviourMyPun.cs
@@ -0,0 +1,7 @@
+﻿using Photon.Pun;
+
+namespace Utils.Behaviours {
+	public class MonoBehaviourMyPun : MonoBehaviourPun {
+		public bool isMine => photonView.IsMine || Equals(photonView.Controller, PhotonNetwork.LocalPlayer);
+	}
+}
