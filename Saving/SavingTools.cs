a15927fbcdb232a6823a7a21eea70d40550c34b5
Nathan <nathan.istace@gmail.com>
2021-01-10 14:28:29 +0100

    Added an utility function to open persistent data path

---
+++ Saving/SavingTools.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Utils.Saving {
+	public static class SavingTools {
+#if UNITY_EDITOR
+
+		[MenuItem("Tools/Saving/Open persistent data folder")]
+		public static void ToolsOpenPersistentDataFolder() {
+			System.Diagnostics.Process.Start("explorer.exe", $"/select,{Application.persistentDataPath.Replace(@"/", @"\")}\\");
+		}
+#endif
+	}
+}
