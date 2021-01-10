3094f9fdb1bc3e3f8e2c0525fdf8c65840c3da2a
Nathan <nathan.istace@gmail.com>
2021-01-10 14:30:33 +0100

    Reviewed ListInputUi to act more like a Unity component

--- Ui/ListInputUi.cs
+++ Ui/ListInputUi.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,60 +8,72 @@ using Utils.Extensions;
 using Utils.Libraries;
 
 namespace Utils.Ui {
-	public class ListInputUi : MonoBehaviour {
-		[SerializeField] protected bool     _interactable = true;
+	public class ListInputUi : Selectable {
 		[SerializeField] protected L10NText _currentItemTextKey;
 		[SerializeField] protected TMP_Text _currentItemTextValue;
 		[SerializeField] protected bool     _optionsAreKeys = true;
 		[SerializeField] protected Button   _previousItemButton;
 		[SerializeField] protected Button   _nextItemButton;
-		[SerializeField] protected bool     _loop = true;
+		[SerializeField] protected bool     _loop    = true;
+		[SerializeField] protected string[] _options = { };
+		[SerializeField] protected int      _value;
 
-		public  bool         interactable => _interactable;
-		private List<string> options      { get; } = new List<string>();
-		private int          currentIndex { get; set; }
+		public new bool interactable {
+			get => base.interactable;
+			set {
+				base.interactable = value;
+				Refresh();
+			}
+		}
+
+		public int value {
+			get => _value;
+			set {
+				SetValueWithoutNotify(value);
+				onValueChanged.Invoke(_value);
+			}
+		}
 
 		public IntEvent onValueChanged { get; } = new IntEvent();
 
-		private void Awake() {
+		protected override void Awake() {
+			base.Awake();
 			Refresh();
 			_nextItemButton.onClick.AddListenerOnce(IncrementIndex);
 			_previousItemButton.onClick.AddListenerOnce(DecrementIndex);
 		}
 
-		public void Set(IEnumerable<string> optionsAsKeys, int currentIndex, bool withoutNotify = true) {
-			options.Clear();
-			options.AddRange(optionsAsKeys);
+		public void Set(IEnumerable<string> options, int currentIndex, bool withoutNotify = true) {
+			_options = options.ToArray();
 			if (withoutNotify) SetValueWithoutNotify(currentIndex);
-			else SetValue(currentIndex);
+			else value = currentIndex;
 			Refresh();
 		}
 
 		private void Refresh() {
-			var hasItems = options.Count > 0;
-			_currentItemTextKey.enabled = _optionsAreKeys;
-			if (_optionsAreKeys) _currentItemTextKey.key = hasItems ? options[currentIndex] : string.Empty;
-			else _currentItemTextValue.text = hasItems ? options[currentIndex] : string.Empty;
-			_previousItemButton.interactable = interactable && hasItems && (_loop || currentIndex > 0);
-			_nextItemButton.interactable = interactable && hasItems && (_loop || currentIndex < options.Count - 1);
+			if (_currentItemTextKey && _currentItemTextKey.enabled != _optionsAreKeys) _currentItemTextKey.enabled = _optionsAreKeys;
+			var valueOption = _options.GetSafe(_value, string.Empty);
+			if (_currentItemTextKey && _optionsAreKeys && _currentItemTextKey.key != valueOption) _currentItemTextKey.key = valueOption;
+			if (_currentItemTextValue && !_optionsAreKeys && _currentItemTextValue.text != valueOption) _currentItemTextValue.text = valueOption;
+			_previousItemButton?.SetInteractableIfDiff(interactable && _options.Length > 1 && (_loop || _value > 0));
+			_nextItemButton?.SetInteractableIfDiff(interactable && _options.Length > 1 && (_loop || _value < _options.Length - 1));
 		}
 
-		public void SetValueWithoutNotify(int index) {
-			currentIndex = index.Clamp(0, options.Count - 1);
+		public void SetValueWithoutNotify(int value) {
+			_value = value;
+			if (_loop) _value = _value.PosMod(_options.Length);
+			_value = _value.Clamp(0, _options.Length - 1);
 			Refresh();
 		}
 
-		public void SetInteractable(bool interactable) {
-			_interactable = interactable;
-			Refresh();
-		}
+		private void IncrementIndex() => value++;
+		private void DecrementIndex() => value--;
 
-		private void IncrementIndex() => SetValue(_loop ? (currentIndex + 1).PosMod(options.Count) : (currentIndex + 1).Clamp(0, options.Count - 1));
-		private void DecrementIndex() => SetValue(_loop ? (currentIndex - 1).PosMod(options.Count) : (currentIndex - 1).Clamp(0, options.Count - 1));
-
-		private void SetValue(int index) {
-			SetValueWithoutNotify(index);
-			onValueChanged.Invoke(currentIndex);
+#if UNITY_EDITOR
+		protected override void OnValidate() {
+			base.OnValidate();
+			SetValueWithoutNotify(_value);
 		}
+#endif
 	}
 }
