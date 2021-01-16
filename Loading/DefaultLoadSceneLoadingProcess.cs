76badba0d12c76bb562a232b25a0dac621077e2e
Nathan <nathan.istace@gmail.com>
2021-01-16 15:26:20 +0100

    DefaultLoadSceneLoadingProcess

---
+++ Loading/DefaultLoadSceneLoadingProcess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Utils.Loading {
+	public class DefaultLoadSceneLoadingProcess : LoadingProcess {
+		public override IEnumerator routine => Do();
+
+		private string sceneName { get; }
+
+		public DefaultLoadSceneLoadingProcess(string sceneName) {
+			this.sceneName = sceneName;
+		}
+
+		private IEnumerator Do() {
+			var operation = SceneManager.LoadSceneAsync(sceneName);
+			while (!operation.isDone) yield return Progress(operation.progress);
+		}
+	}
+}
