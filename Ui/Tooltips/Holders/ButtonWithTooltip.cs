f947af7b3bd70ee21ef7905e7161e093c9ad17a5
Nathan <nathan.istace@gmail.com>
2021-01-28 10:02:46 +0100

    Shortcut for a button with a tooltip

---
+++ Ui/Tooltips/Holders/ButtonWithTooltip.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.UI;
+using Utils.Extensions;
+
+namespace Utils.Ui.Tooltips {
+	public class ButtonWithTooltip : Button {
+		private FixedTooltipHolder sTooltip { get; set; }
+		public  FixedTooltipHolder tooltip  => sTooltip ? sTooltip : (sTooltip = gameObject.GetOrAddComponent<FixedTooltipHolder>());
+	}
+}
