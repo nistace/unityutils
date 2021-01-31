763dc933f8dcb26939d624830dc2e3a4202a208d
Nathan <nathan.istace@gmail.com>
2021-01-31 10:44:47 +0100

    Added a wrapper to the single coroutine to check whether it's running or not

--- Coroutines/SingleCoroutine.cs
+++ Coroutines/SingleCoroutine.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 namespace Utils.Coroutines {
 	public class SingleCoroutine {
-		private MonoBehaviour actor         { get; }
-		private Coroutine     singleRoutine { get; set; }
+		private MonoBehaviour actor     { get; }
+		private Coroutine     coroutine { get; set; }
+		public  bool          running   { get; private set; }
 
 		public SingleCoroutine(MonoBehaviour actor) {
 			this.actor = actor;
@@ -13,15 +14,23 @@ namespace Utils.Coroutines {
 		public void Start(IEnumerator routine) {
 			if (!actor) return;
 			if (!actor.isActiveAndEnabled) return;
-			Stop();
-			singleRoutine = actor.StartCoroutine(routine);
+			if (running) Stop();
+			coroutine = actor.StartCoroutine(RoutineWrapper(routine));
 		}
 
 		public void Stop() {
 			if (!actor) return;
 			if (!actor.enabled) return;
-			if (singleRoutine == null) return;
-			actor.StopCoroutine(singleRoutine);
+			if (!running) return;
+			if (coroutine == null) return;
+			actor.StopCoroutine(coroutine);
+			running = false;
+		}
+
+		private IEnumerator RoutineWrapper(IEnumerator routine) {
+			running = true;
+			while (routine != null && routine.MoveNext()) yield return routine.Current;
+			running = false;
 		}
 	}
 }
