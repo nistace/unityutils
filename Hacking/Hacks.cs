12683a2040c41a2c36b331ed40d6280f6a4bc268
Nathan <nathan.istace@gmail.com>
2021-03-06 13:41:51 +0100

    Added utils to prevent from game speed hacks.

---
+++ Hacking/Hacks.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine.Events;
+
+namespace Utils.Hacking {
+	public static class Hacks {
+		public static UnityEvent onHackDetected { get; } = new UnityEvent();
+	}
+}
---
+++ Hacking/SpeedHackDetector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils.Hacking {
+	public class SpeedHackDetector : MonoBehaviour {
+		[SerializeField] protected int   _timeDiff;
+		[SerializeField] protected int   _previousTime;
+		[SerializeField] protected int   _realTime;
+		[SerializeField] protected float _gameTime;
+		[SerializeField] protected bool  _detected;
+		[SerializeField] protected int   _threshold;
+
+		public static void Instantiate(int threshold = 7) => new GameObject("SpeedHackDetector").AddComponent<SpeedHackDetector>()._threshold = threshold;
+
+		private void Start() {
+			_previousTime = DateTime.Now.Second;
+			_gameTime = 0;
+		}
+
+		private void Update() {
+			_gameTime += Time.deltaTime;
+			if (_previousTime == DateTime.Now.Second) return;
+			_realTime++;
+			_previousTime = DateTime.Now.Second;
+			_timeDiff = (int) _gameTime - _realTime;
+			if (_timeDiff > _threshold) {
+				if (_detected) return;
+				_detected = true;
+				Hacks.onHackDetected.Invoke();
+			}
+			else _detected = false;
+		}
+	}
+}
