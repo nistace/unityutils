1f9638a3fd70040e5e9880d0634d28ab3a68a759
Nathan <nathan.istace@gmail.com>
2021-03-17 10:35:05 +0100

    Component to pingpong-move a ui element

---
+++ Ui/PingPongUi.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Utils.Ui {
+	public class PingPongUi : MonoBehaviourUi {
+		[SerializeField] protected Vector2 _first;
+		[SerializeField] protected Vector2 _second;
+		[SerializeField] protected float   _speed = 1;
+
+		private void Update() => transform.localPosition = Vector2.Lerp(_first, _second, (Mathf.Sin(Time.time * _speed) + 1) / 2);
+	}
+}
