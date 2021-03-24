18bf15cc441d874f9cb94a45901edc04f10cf807
Nathan <nathan.istace@gmail.com>
2021-03-24 14:46:55 +0100

    Fixed importing csv + sort keys in localisation files

--- Libraries/Localisation/Language.cs
+++ Libraries/Localisation/Language.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
@@ -37,32 +36,7 @@ namespace Utils.Libraries {
 			foreach (var textAsset in Resources.LoadAll<TextAsset>("Localisation")) {
 				if (textAsset.name.In("allTexts", "how-to")) continue;
 				var path = AssetDatabase.GetAssetPath(textAsset);
-				var allLines = textAsset.Lines().Select(t => t.Replace("\n", string.Empty).Trim()).Where(t => !string.IsNullOrEmpty(t)).ToList();
-
-				using (var writer = new StreamWriter(path, false)) {
-					while (allLines.Count > 0) {
-						while (allLines.Count > 0 && allLines[0].StartsWith("#")) {
-							writer.WriteLine(allLines[0]);
-							allLines.RemoveAt(0);
-						}
-						var blockLines = new List<string>();
-						while (allLines.Count > 0 && !allLines[0].StartsWith("#")) {
-							blockLines.Add(allLines[0]);
-							allLines.RemoveAt(0);
-						}
-						var previousLineKey = string.Empty;
-						foreach (var line in blockLines.OrderBy(t => t)) {
-							if (line.IndexOf("=", StringComparison.Ordinal) < 0) continue;
-							var key = line.Substring(0, line.IndexOf("=", StringComparison.Ordinal)).CleanKey();
-							if (previousLineKey == key) {
-								Debug.Log("Duplicate entry for key " + key);
-							}
-							previousLineKey = key;
-							writer.WriteLine(line);
-						}
-						writer.WriteLine(string.Empty);
-					}
-				}
+				WriteSortedLines(path, textAsset.Lines().Select(t => t.Replace("\n", string.Empty).Trim()).Where(t => !string.IsNullOrEmpty(t)).ToList());
 				AssetDatabase.ImportAsset(path);
 				EditorUtility.SetDirty(textAsset);
 				Debug.Log($"Keys sorted in {textAsset.name} asset file.");
@@ -92,21 +66,44 @@ namespace Utils.Libraries {
 					langTextAsset = Resources.Load<TextAsset>($"Localisation/{lang.CamelCase()}");
 					Debug.Log($"Assets/Resources/Localisation/{lang.CamelCase()}.properties created.");
 				}
+				var path = AssetDatabase.GetAssetPath(langTextAsset);
 
-				var buffer = new StringBuilder();
+				var langLines = lines.Select(t => (key: t.GetSafe(keyColumnIndex, string.Empty).Trim(), value: t.GetSafe(langColumnIndex, string.Empty).Trim()))
+					.Where(t => !string.IsNullOrEmpty(t.key) && (t.key.StartsWith("#") || !string.IsNullOrEmpty(t.value))).Select(t => t.key.StartsWith("#") ? t.key : $"{t.key} = {t.value}");
 
-				foreach (var line in lines) {
-					if (line[keyColumnIndex].StartsWith("#")) buffer.Append(Environment.NewLine).Append(line[keyColumnIndex]).Append(Environment.NewLine);
-					else if (!string.IsNullOrEmpty(line[keyColumnIndex]) && line.Length > langColumnIndex && !string.IsNullOrEmpty(line[langColumnIndex]))
-						buffer.Append(line[keyColumnIndex].CleanKey(true)).Append(" = ").Append(line[langColumnIndex]).Append(Environment.NewLine);
-				}
-				File.WriteAllText(AssetDatabase.GetAssetPath(langTextAsset), buffer.ToString());
+				WriteSortedLines(path, langLines);
+				AssetDatabase.ImportAsset(path);
 				EditorUtility.SetDirty(langTextAsset);
 				Debug.Log($"{lang.CamelCase()}.properties updated.");
 			}
 			AssetDatabase.SaveAssets();
-			SortLinesInFiles();
 		}
+
+		private static void WriteSortedLines(string path, IEnumerable<string> langLines) {
+			var linesAsList = langLines.ToList();
+			using (var writer = new StreamWriter(path, false)) {
+				while (linesAsList.Count > 0) {
+					while (linesAsList.Count > 0 && linesAsList[0].StartsWith("#")) {
+						writer.WriteLine(linesAsList[0]);
+						linesAsList.RemoveAt(0);
+					}
+					var blockLines = new List<string>();
+					while (linesAsList.Count > 0 && !linesAsList[0].StartsWith("#")) {
+						blockLines.Add(linesAsList[0]);
+						linesAsList.RemoveAt(0);
+					}
+					var previousLineKey = string.Empty;
+					foreach (var line in blockLines.OrderBy(t => t)) {
+						var key = line.Substring(0, line.IndexOf("=", StringComparison.Ordinal)).CleanKey();
+						if (previousLineKey == key) Debug.Log("Duplicate entry for key " + key);
+						previousLineKey = key;
+						writer.WriteLine(line);
+					}
+					writer.WriteLine(string.Empty);
+				}
+			}
+		}
+
 #endif
 	}
 }
