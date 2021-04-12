7d4d93f33efd1b35a32cc758f25b189ea6067285
Nathan <nathan.istace@gmail.com>
2021-04-12 09:01:39 +0200

    Fixed warning message

--- Libraries/Particles/ParticlesLibrary.cs
+++ Libraries/Particles/ParticlesLibrary.cs
@@ -5,7 +5,7 @@ using Utils.Extensions;
 namespace Utils.Libraries {
 	[CreateAssetMenu(menuName = "Constants/Libraries/Particles")]
 	public class ParticlesLibrary : Library<ParticleSystem> {
-		protected override string GetNonExistingWarningMessage(string key) => $"No color value for the key {key} in the particles library {name}";
+		protected override string GetNonExistingWarningMessage(string key) => $"No particle value for the key {key} in the particles library {name}";
 
 #if UNITY_EDITOR
 
