c762caa449adf2eea68e396d757596889de5ab58
Nathan <nathan.istace@gmail.com>
2021-04-21 22:13:27 +0200

    Moved Pun utils to a distinct git repository

--- Saving/ISaveToHashtable.cs
+++
@@ -1,7 +0,0 @@
-﻿using ExitGames.Client.Photon;
-
-namespace Utils.Saving {
-	public interface ISaveToHashtable {
-		void Save(Hashtable data, string root);
-	}
-}
--- StaticUtils/PunUtils.cs
+++
@@ -1,7 +0,0 @@
-﻿using Photon.Pun;
-
-namespace Utils.StaticUtils {
-	public static class PunUtils {
-		public static bool offlineOrNoRoom => PhotonNetwork.OfflineMode || PhotonNetwork.CurrentRoom == null;
-	}
-}
