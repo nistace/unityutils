8c1aad4eaefd39604c1cf719628771763d8d8bba
Nathan <nathan.istace@gmail.com>
2021-05-07 14:11:36 +0200

    Added utils to convert deg angles between 0 - 360 to -180 - 180 and vice versa

---
+++ StaticUtils/MathUtils.cs
@@ -0,0 +1,12 @@
+﻿using Utils.Extensions;
+
+namespace Utils.StaticUtils {
+	public static class MathUtils {
+		public static float ToUnsignedDegAngle(float degAngle) => degAngle.PosMod(360);
+
+		public static float ToSignedDegAngle(float degAngle) {
+			var unsignedAngle = ToUnsignedDegAngle(degAngle);
+			return unsignedAngle > 180 ? unsignedAngle - 360 : unsignedAngle;
+		}
+	}
+}
