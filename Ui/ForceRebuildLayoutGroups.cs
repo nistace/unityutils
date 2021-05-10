bc3a1444eb637c1def0f490f6bb1dede3706d98a
Nathan <nathan.istace@gmail.com>
2021-05-10 10:56:00 +0200

    ForceRebuildLayoutGroups is added to a set of layout groups to force them to refresh during multiple frames

---
+++ Ui/ForceRebuildLayoutGroups.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Utils.Coroutines;
+using Utils.Extensions;
+
+namespace Utils.Ui {
+	public class ForceRebuildLayoutGroups : MonoBehaviourUi {
+		[SerializeField] protected int _frames = 2;
+
+		private SingleCoroutine singleCoroutine { get; set; }
+		private LayoutGroup[]   layoutGroups    { get; set; }
+
+		public void Play(int? frames = null) {
+			if (singleCoroutine == null) singleCoroutine = new SingleCoroutine(this);
+			layoutGroups = GetComponentsInChildren<LayoutGroup>();
+			singleCoroutine.Start(DoPlay(frames ?? _frames));
+		}
+
+		private IEnumerator DoPlay(int frames) {
+			for (var i = 0; i < frames; ++i) {
+				layoutGroups.ForEach(t => t.enabled = false);
+				layoutGroups.ForEach(t => t.enabled = true);
+				yield return null;
+			}
+		}
+	}
+}
