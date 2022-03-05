a9ae5126cdf842ec2bf9428fa56e401956b3fe0e
Nathan <nathan.istace@gmail.com>
2022-03-05 20:04:05 +0100

    Added the possibility to have a smooth follow

--- Behaviours/Follow.cs
+++ Behaviours/Follow.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Follow : MonoBehaviour {
 	[SerializeField] protected Transform _target;
 	[SerializeField] protected Vector3   _offset;
+	[SerializeField] protected float     _smoothTime = 1;
+	[SerializeField] protected Vector3   _velocity;
 
 	public Transform target {
 		get => _target;
@@ -16,6 +18,6 @@ public class Follow : MonoBehaviour {
 
 	private void LateUpdate() {
 		if (!_target) return;
-		transform.position = _target.position + _offset;
+		transform.position = Vector3.SmoothDamp(transform.position, _target.position + _offset, ref _velocity, _smoothTime);
 	}
 }
