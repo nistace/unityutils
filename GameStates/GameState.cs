4c482b662cc7b4da209ea7f91c34093bf37e39c2
Nathan Istace <nathan.istace@gmail.com>
2023-04-30 02:26:58 +0200

    Made it easier to load libraries by using one scriptable object that targets all of them. Added functions in GameState to easily have an Update loop equivalent and enable/disable event listeners

--- GameStates/GameState.cs
+++ GameStates/GameState.cs
@@ -1,17 +1,37 @@
-﻿namespace Utils.GameStates {
+﻿using System.Collections;
+using UnityEngine;
+using Utils.Coroutines;
+
+namespace Utils.GameStates {
 	public abstract class GameState {
-		public static GameState currentState  { get; private set; }
-		public static GameState previousState { get; private set; }
+		public static  GameState currentState  { get; private set; }
+		public static  GameState previousState { get; private set; }
+		private static Coroutine stateRoutine  { get; set; }
+
+		protected bool enabled { get; private set; }
 
 		protected abstract void Enable();
 		protected abstract void Disable();
+		protected abstract IEnumerator Continue();
+		protected abstract void SetListenersEnabled(bool enabled);
 
 		public static void ChangeState(GameState newState, bool force = false) {
 			if (!force && currentState == newState) return;
-			currentState?.Disable();
+			if (currentState != null) {
+				currentState.enabled = false;
+				currentState.SetListenersEnabled(false);
+				if (stateRoutine != null) CoroutineRunner.Stop(stateRoutine);
+				stateRoutine = null;
+				currentState.Disable();
+			}
 			previousState = currentState;
 			currentState = newState;
-			currentState?.Enable();
+			if (currentState != null) {
+				currentState.Enable();
+				currentState.enabled = true;
+				currentState.SetListenersEnabled(true);
+				stateRoutine = CoroutineRunner.Run(currentState.Continue());
+			}
 		}
 
 		public static void ChangeStateToPrevious(bool force = false) => ChangeState(previousState, force);
---
+++ Libraries/LibraryBuildData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Utils.Extensions;
+
+namespace Utils.Libraries {
+	[CreateAssetMenu]
+	public class LibraryBuildData : ScriptableObject {
+		[SerializeField] protected ColorLibrary[]     _colorLibraries;
+		[SerializeField] protected CursorLibrary[]    _cursorLibraries;
+		[SerializeField] protected AudioClipLibrary[] _audioClipLibraries;
+		[SerializeField] protected ParticlesLibrary[] _particlesLibraries;
+		[SerializeField] protected SpriteLibrary[]    _spriteLibraries;
+
+		public void Build() {
+			_colorLibraries.ForEach(Colors.LoadLibrary);
+			_cursorLibraries.ForEach(CursorManager.LoadLibrary);
+			_audioClipLibraries.ForEach(AudioClips.LoadLibrary);
+			_particlesLibraries.ForEach(Particles.LoadLibrary);
+			_spriteLibraries.ForEach(Sprites.LoadLibrary);
+		}
+	}
+}
