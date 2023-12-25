22cb28c4e7df62cc82ce456862a4eafa74bf70c9
Nathan Istace <nathan.istace@gmail.com>
2023-12-25 16:57:27 +0100

    Added function to get physical address of the device

---
+++ StaticUtils/DeviceUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.NetworkInformation;
+
+namespace NiUtils.StaticUtils {
+	public static class DeviceUtils {
+		public static string GetPhysicalAddress() {
+			var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+			foreach (var adapter in networkInterfaces) {
+				var address = adapter.GetPhysicalAddress();
+				if (!string.IsNullOrEmpty(address.ToString())) {
+					return address.ToString();
+				}
+			}
+			return string.Empty;
+		}
+	}
+}
