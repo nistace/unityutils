55070e93970fa53849e31f728d62a22ad3f42f5c
Nathan Istace <nathan.istace@gmail.com>
2024-01-13 16:58:38 +0100

    Mobile keyboard utils

---
+++ StaticUtils/TouchScreenKeyboards.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace NiUtils.StaticUtils {
+	public static class TouchScreenKeyboards {
+		public static TouchScreenKeyboard CurrentKeyboard { get; set; }
+
+		public static int GetKeyboardHeight() {
+#if UNITY_ANDROID && !UNITY_EDITOR
+			using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+				var View = unityClass.GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer").Call<AndroidJavaObject>("getView");
+
+				using (var Rct = new AndroidJavaObject("android.graphics.Rect")) {
+					View.Call("getWindowVisibleDisplayFrame", Rct);
+
+					return Screen.height - Rct.Call<int>("height");
+				}
+			}
+#endif
+
+			return 0;
+		}
+
+		public static float GetKeyboardHeightRatio() => (float)GetKeyboardHeight() / Screen.height;
+	}
+}
